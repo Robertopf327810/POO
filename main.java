1feb68262d53a856335bb84aa8d446f9e7a9d1df
Robertopf327810 <robertopf327@gmail.com>
2025-08-14 19:55:51 -0600

    hdfjbdfjc

--- main.java
+++ main.java
@@ -1,5 +1,56 @@
+
+import java.util.Scanner;
+
+
+class Alumno {
+
+    public String name;
+    public int enrolled;
+    public double p1;
+    public double p2;
+    public double p3;
+
+    public Alumno(String name, int enrolled, double p1, double p2, double p3){
+        this.name = name;
+        this.enrolled = enrolled;
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+                
+    }
+
+    public String getName(){
+        return name;
+
+    }
+
+    public int getenrolled(){
+        return enrolled;
+
+    }
+
+    public double getp1(){
+        return p1;
+
+    }
+
+    public double getp2(){
+        return p2;
+
+    }
+
+    public double getp3(){
+        return p3;
+
+    }
+}
+
+
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Que quieres hacer:\n 1.-");
     }
 }
