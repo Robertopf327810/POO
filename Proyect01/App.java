34169d73ce14e728536ec05231dc21d4e4ccab92
Robertopf327810 <robertopf327@gmail.com>
2025-08-20 18:56:09 -0600

    Proyect02&01

--- Proyect01/App.java
+++ Proyect01/App.java
@@ -1,5 +1,95 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Alumno {
+
+    public String name;
+    public int enrolled;
+    public double p1;
+    public double p2;
+    public double p3;
+
+    public Alumno(String name, int enrolled, double p1, double p2, double p3){
+        this.name = name;
+        this.enrolled = enrolled;
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getenrolled(){
+        return enrolled;
+    }
+
+    public double getp1(){
+        return p1;
+    }
+
+    public double getp2(){
+        return p2;
+    }
+
+    public double getp3(){
+        return p3;
+    }
+}
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean cycle = true;
+        ArrayList<Alumno> alumnos = new ArrayList<>();
+
+        while(cycle){
+            System.out.println("Select an option:\n 1.-Register alumn \n 2.-Print students with their qualifications \n 3.-Exit");
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            Double Final;
+            switch(option){
+                case 1:
+                    // Register a new student
+                    System.out.print("Enter name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter enrollment number: ");
+                    int enrolled = sc.nextInt();
+                    System.out.print("Enter grade for P1: ");
+                    double p1 = sc.nextDouble();
+                    System.out.print("Enter grade for P2: ");
+                    double p2 = sc.nextDouble();
+                    System.out.print("Enter grade for P3: ");
+                    double p3 = sc.nextDouble();
+                    sc.nextLine();
+                    Alumno alumno = new Alumno(name, enrolled, p1, p2, p3);
+                    alumnos.add(alumno);
+
+                    System.out.println("The alumn has registered!\n");
+                    break;
+                case 2:
+                    if(alumnos.isEmpty()){
+                        System.out.println("No students registered.\n");
+                    } else {
+                        for(Alumno a : alumnos){
+                            System.out.println("Name: " + a.getName() + ", Enrollment: " + a.getenrolled() +
+                                ", P1: " + a.getp1() + ", P2: " + a.getp2() + ", P3: " + a.getp3() +
+                                ", Final Grade: " + ((a.getp1()*0.25) + (a.getp2()*0.25) + (a.getp3()*0.5)));
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    cycle = false;
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid option.\n");
+                    System.out.println("Please select a valid option.");
+                    break;
+            }
+        }
     }
 }
